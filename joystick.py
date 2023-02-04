f1f401256ef94f38897f215e4ae2a7fde5619e65
may-sun128 <masondaleholmes@gmail.com>
2023-02-04 02:02:10 +0000

    get non-axis buttons working with evdev

--- joystick.py
+++ joystick.py
@@ -1,4 +1,17 @@
+import logging
+import evdev
+from evdev import InputDevice, categorize, ecodes
+
+
 class JoyStick:
+	def __init__(self):
+		self.devices = [InputDevice(path) for path in evdev.list_devices()]
+		for device in self.devices:
+			if device.name == 'N/A':
+				break
+		# Attach to the joystick device
+		self.joystick = InputDevice(device.path)
+	
 	def a_button_pressed():
 		pass
 	def b_button_pressed():
@@ -16,11 +29,59 @@ class JoyStick:
 	def left_trigger_pressed():
 		pass
 
+	def process_events(self):
+		for event in self.joystick.read_loop():
+			match event.value:
+				# if pressed
+				case 1:
+					match event.code:
+						case 305:
+							self.a_button_pressed()
+						case 306:
+							self.b_button_pressed()
+						case 304:
+							self.x_button_pressed()
+						case 307:
+							self.y_button_pressed()
+						case 312:
+							self.select_button_pressed()
+						case 313:
+							self.start_button_pressed()
+						case 308:
+							self.left_trigger_pressed()
+						case 309:
+							self.right_trigger_pressed()
+				# If released
+				case 0:
+					match event.code: 
+						case 305:
+							# print('A Button was released')
+							pass
+						# &c 
 
-# def hello_world():
-# 	print('hello world')
+	def __print_events(self):
+		for event in joystick.read_loop():
+			print((categorize(event)))
+	
+	def __print_event_info(self, input_event):
+		print(f'Type: {input_event.type}')
+		print(f'Code: {input_event.code}')
+		print(f'Value: {input_event.value}')
+		print('\n')
 
-# js = JoyStick()
-# js.a_button_pressed = hello_world
 
-# js.a_button_pressed()
+js = JoyStick()
+
+def hello_world():
+	print('Hello World')
+
+js.a_button_pressed = hello_world
+js.b_button_pressed = hello_world
+js.x_button_pressed = hello_world
+js.y_button_pressed = hello_world
+js.right_trigger_pressed = hello_world
+js.left_trigger_pressed = hello_world
+js.start_button_pressed = hello_world
+js.select_button_pressed = hello_world
+
+js.process_events()
