6200c0f246aed52cb7f5cfce46f9981a363dbfeb
may-sun128 <masondaleholmes@gmail.com>
2023-02-05 18:44:10 +0000

    add simple configuration file writting from form

--- configuration-form.py
+++ configuration-form.py
@@ -1,26 +1,47 @@
 from nicegui import ui
 import joystick
 import os
+import json 
 
-# TODO figure out how to import joystick module 
+# TODO figure out how to import joystick module from another directory 
 
 
-js = joystick.JoyStick()
+# write button dictionary to ./config.json
+def write_config(config: dict):
+    json_config = json.dumps(config, indent = 4)
+    with open('config.json', 'w') as f:
+        f.write(json_config)
 
-def get_joystick(result):
-    js.a_button_pressed = lambda: os.system(result)
-
+# non-dpad buttons 
 ui.markdown('## Bind Buttons')
+
+# A Button 
+
+button_dict = {
+        'a button': {
+            'type': '', 
+            'value': '',
+            'event': ''
+        }
+    }
+
+# hard code event 
+button_dict.update({'event': 'pressed'})
+# label
 ui.label('A Button')
-a_button_select = ui.select(['Bash', 'Script', 'HotKey', 'Mouse Action'], value="Initial")
-ui.input(label='Text', placeholder='Enter bash code...')
-result = ui.label()
-print(f'ui label result = {ui.label}')
-ui.button('Go', on_click=lambda: get_joystick(result))
+# get type (bash, script, hotkey, mouse action)
+a_button_drp_dwn = ui.select(['Bash', 'Script', 'HotKey', 'Mouse Action'], value='Initial', on_change=lambda: button_dict.update({'type': a_button_drp_dwn.value}))
+# get value ('echo hello world', '~/scripts/hello-world.sh', hotkey, &c)
+a_button_txt_bx = ui.input(label='Text', placeholder='Enter bash code...', on_change=lambda: button_dict.update({'value': a_button_txt_bx.value}))
+# write dict to file 
+ui.button('Save', on_click=lambda: write_config(button_dict))
+# start 
+ui.run(dark=True, show=False)
 
-ui.run()
 
-# js.process_events()
+
+
+
 
 '''
     GUI LAYOUT
@@ -29,5 +50,19 @@ ui.run()
         drop down for hotkey, inline bash, or path to script 
             re path to script, see if nicegui has an option for opening files 
         
+
+    d: dict = {
+        'type': 'bash' or 'script' or 'hotkey' or 'mouse action'
+        'value': 
+    }
+
+    inline_bash = {
+        'a button': {
+            'type': 'bash', 
+            'value': 'echo hello world',
+            'button': 'a', 
+            'event': 'pressed'
+        }
+    }
 
 '''
